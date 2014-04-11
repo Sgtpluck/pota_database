f39be72b3bb6e22b49fd87d61d6e370688ada5c2
Davida Marion <davidamarion@gmail.com>
2014-04-11 13:32:52 -0700

    grabs most recent collections by uniq serial number

--- app/models/data_finder.rb
+++ app/models/data_finder.rb
@@ -5,7 +5,20 @@ class DataFinder
 
   def self.all_unique_serial_ids
    # grab the most recent of all the unique items
-   
+    find_uniq_ids
+    @most_recent_ids.map do |id|
+      WaterData.find(id)
+    end
   end
 
+  def self.find_uniq_ids
+    @most_recent_ids = []
+    serial_number = []
+    WaterData.all.order('time ASC').each do |data|
+      unless serial_number.include? data.serial
+        @most_recent_ids << data.id
+        serial_number << data.serial
+      end
+    end
+  end
 end
